640c5717b89a964d68678da29c2981a64a7ffa20
fakhryhizballah <falehry88@gmail.com>
2020-11-01 17:31:02 +0700

    Migration Data Tabel

---
+++ app/Database/Migrations/2020-11-01-102403_TB_Histori.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TBHistori extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => TRUE,
+				'auto_increment' => TRUE
+			],
+			'ID_User'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '225',
+			],
+
+			'created_at'       => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'updated_at'       => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+		]);
+		$this->forge->addKey('id', TRUE);
+		$this->forge->createTable('TB_Histori');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('TB_Histori');
+	}
+}
